3ce5296dded062ac58032324b7fed19bea821f9b
Sina Hajipour <dev.sinahajipour@gmail.com>
2024-04-07 22:26:40 +0330

    add https agent for axios

--- src/utils/axios/axios.ts
+++ src/utils/axios/axios.ts
@@ -1,5 +1,7 @@
 import axios from 'axios';
 
+// import https from 'https'
+
 const BASE_API_URL = 'http://192.168.2.102:81/api';
 export const BASE_URL = 'http://192.168.2.102:81';
 
@@ -10,6 +12,11 @@ const axiosConfig = axios.create({
         'Content-Type': 'application/json',
         'Accept': 'application/json',
     },
+
+    // // add this for https request
+    //     httpsAgent: new https.Agent({
+    //         rejectUnauthorized: false
+    //     }),
 
 });
 
